e4d3f34650c0d0b45d0e0546af2ab52c3171943c
Pablo Polacco <pablopolacco@gmail.com>
2023-09-27 00:25:18 -0300

    implementado carrito y actualizacion de stock

--- preentrega3.js
+++ preentrega3.js
@@ -26,11 +26,13 @@ titulo.textContent ="Bienvenido a Pocima De Luna"
 
 
 
-// mando a json Productos y lo armo con una img
+// mando a json Productos 
 // lo armo con json.stringify(productos)
 const productosAlaventa = [new Producto("Sahumerio", 1500, 20), new Producto("Vela", 2500, 5), new Producto("Perfume", 3500, 15)]
 
 
+
+//localStorage.setItem("prouctosSeleccionados", productosJson)
 
 const subtitulo = document.createElement("h3")
 subtitulo.textContent = "Los productos disponibles son:"
@@ -46,12 +48,14 @@ productosAlaventa.forEach( p => {
     card.innerHTML = `<div class="card">
                             <h5 class="card-title">${p.nombre} "JAZMÍN"</h5>
                             <p class="card-text">Precio: $ ${p.precio}</p>
+                            <p class="card-stock">Sock: ${p.stock} </p>
                             <button class="btnProd" value=${p.nombre}> Agregar al carrito </button>
                         </div>`
     articulos.append(card);
 })
-   
-let carrito = {}
+
+body.appendChild(articulos)
+let carrito = []
 
 let botones = document.querySelectorAll(".btnProd")
 
@@ -60,7 +64,37 @@ for (var i = 0; i < botones.length; i++) {
 }
 
 function agregarACarrito(e){
-    console.log(e.target.val)
+    let boton = e.target
+    let card = boton.parentNode
+    console.log(e.target)
+    console.log(card)
+    console.log(boton.value)
+    
+
+   
+   let producto = productosAlaventa.find(p=> p.nombre == boton.value)
+    console.log(producto)
+   for (let i = 0; productosAlaventa.length; i++)
+   {
+    // buscar el producto en el listado..
+    if (productosAlaventa[i].nombre == boton.value){
+        //actualizo el stock
+        console.log(productosAlaventa[i].stock - 1)
+        productosAlaventa[i].stock = productosAlaventa[i].stock -1
+        //lo reflejo en el html
+        let p = card.querySelector(".card-stock")
+        p.textContent = "stock: " + (productosAlaventa[i].stock)
+
+        // agregarlo a carrito y de carrito al storage
+        carrito.push({"producto":productosAlaventa[i].nombre, "precio": productosAlaventa[i].precio, "cantidad":1})
+        // la cantidad la tengo que calcular
+        localStorage.setItem("Carrito", JSON.stringify(carrito) )
+        break;
+    }
+    
+   }
+   
+
 
 }
 
